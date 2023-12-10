f4201d745c49580f8e8cf630841305e009ad7475
JEDI455 <104777978+JEDI455@users.noreply.github.com>
2023-12-09 20:15:15 -0500

    Email authentication through console implemented

--- PaceCRM/settings.py
+++ PaceCRM/settings.py
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = "static_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -124,3 +130,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'leads.User'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.views import generic
@@ -42,6 +43,16 @@ class LeadCreateView(generic.CreateView):
     
     def get_success_url(self) -> str:
         return reverse("leads:lead-list")
+    
+    def form_valid(self, form):
+        send_mail(
+            subject = "A lead has been created", 
+            message= "Go to site to see a new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView, self).form_valid(form)
+    
 
 def lead_create(request):
     form = LeadModelForm()
